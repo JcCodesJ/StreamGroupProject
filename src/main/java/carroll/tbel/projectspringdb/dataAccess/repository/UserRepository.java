4a726de0cd3fd9548c5964b22b7f7182c4ab38e4
Justin <justin.t.carroll1@gmail.com>
2021-10-05 09:23:08 +0200

    Changes by francois

--- src/main/java/carroll/tbel/projectspringdb/dataAccess/repository/UserRepository.java
+++ src/main/java/carroll/tbel/projectspringdb/dataAccess/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package carroll.tbel.projectspringdb.dataAccess.repository;
 
-public interface UserRepository {
+import carroll.tbel.projectspringdb.dataAccess.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findOneByUsernameAndPassword(String username, String password);
 }
--- src/main/java/carroll/tbel/projectspringdb/metier/service/userServ/UserServiceImpl.java
+++ src/main/java/carroll/tbel/projectspringdb/metier/service/userServ/UserServiceImpl.java
@@ -1,4 +1,49 @@
 package carroll.tbel.projectspringdb.metier.service.userServ;
 
-public class UserServiceImpl {
+
+import carroll.tbel.projectspringdb.dataAccess.entity.User;
+import carroll.tbel.projectspringdb.dataAccess.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserRepository repository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public List<User> findAll() {
+        return this.repository.findAll();
+    }
+
+    @Override
+    public Optional<User> findOneById(Long id) {
+        return this.repository.findById(id);
+    }
+
+    @Override
+    public User findOneByLogin(String username, String password) {
+        System.out.println(this.repository.count());
+        return this.repository.findOneByUsernameAndPassword(username, password);
+    }
+
+    @Override
+    public User updateOne(Long id, User user) {
+        if (user.getId() != null) {
+            return this.repository.save(user);
+        }
+        User u = this.repository.findById(id).get();
+        u.setUsername(user.getUsername());
+        u.setPassword(user.getPassword());
+        u.setCreatedAt(user.getCreatedAt());
+
+        return this.repository.save(u);
+    }
 }
